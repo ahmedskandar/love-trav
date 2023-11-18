4b114244aa82826b15eed1a068205eb2be623187
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-18 18:10:41 +0300

    fix: Resolve Header Overlap in Landscape Mode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,9 +14,9 @@ import {
 
 const Home = () => {
   return (
-    <div className="relative flex h-[100svh] flex-col bg-[url(https://images.unsplash.com/photo-1589519160732-57fc498494f8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-top">
+    <div className="relative flex min-h-[100svh] flex-col bg-[url(https://images.unsplash.com/photo-1589519160732-57fc498494f8?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-top">
       <div className="absolute left-0 top-0 h-full w-full bg-black/60 xl:hidden"></div>
-      <header className="absolute left-0 top-0 z-40 w-full">
+      <header className=" left-0 top-0 z-40 w-full">
         <div className="mx-auto flex max-w-xl items-center justify-between p-5 lg:max-w-2xl xl:mx-0 xl:max-w-none xl:px-14 xl:py-5">
           <Image
             src="/assets/icons/love-trav.png"
@@ -62,7 +62,7 @@ const Home = () => {
           </div>
         </div>
       </header>
-      <main className="z-10 mx-auto flex h-full max-w-xl flex-col justify-center p-5 py-10 text-center text-white lg:max-w-2xl xl:mx-0 xl:mt-10 xl:w-1/2 xl:max-w-none xl:p-14 xl:text-left">
+      <main className="z-10 mx-auto flex min-h-[70svh] max-w-xl flex-col justify-center p-5 py-10 text-center text-white lg:max-w-2xl xl:mx-0 xl:w-1/2 xl:max-w-none xl:p-14 xl:text-left">
         <h1 className="font-serif text-4xl lg:text-5xl xl:text-6xl">
           <span className="bg-gradient-to-r from-yellow-500 to-pink-400 bg-clip-text font-bold text-transparent">
             Map
