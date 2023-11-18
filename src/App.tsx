638db05666da5fc697ece63bc2977b32f4f49a8b
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-18 09:38:43 +0300

    feat: Implement Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { Button } from "@nextui-org/button";
+import { NextUIProvider } from "@nextui-org/react";
+import { useNavigate, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <>
-      <Button>Press me</Button>
-    </>
+    <NextUIProvider navigate={navigate}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<p>Login</p>} />
+        <Route path="signup" element={<p>Signup</p>} />{" "}
+        <Route path="reset" element={<p>Reset</p>} />
+        <Route path="app" element={<p>App</p>}>
+          <Route index element={<Navigate to={"travels"} replace />} />
+          <Route path="form" element={<p>Form</p>} />
+          <Route path="travels" element={<p>Travels</p>} />
+        </Route>
+        <Route path="*" element={<p>404, No page found</p>} />
+      </Routes>
+    </NextUIProvider>
   );
 }
 
