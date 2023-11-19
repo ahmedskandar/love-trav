e92adb74f1fa1219106578d25ea2020cc4114b16
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-19 19:02:33 +0300

    fix: Process not defined

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import { apiKey } from "../data/constants";
+
 const supabaseUrl = "https://dzypyxpbrupmlvqsxdmw.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY!; //! will tell TypeScript that process.env.SUPABASE_KEY is not null or undefined
+const supabaseKey = apiKey; //! will tell TypeScript that process.env.SUPABASE_KEY is not null or undefined
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
