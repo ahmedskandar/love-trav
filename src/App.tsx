e25007067d559335474b1031a9fc523646b9d7b9
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-21 10:03:39 +0300

    feat: Implement query client,  provider, devtool

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,36 @@ import { NextUIProvider } from "@nextui-org/react";
 import { useNavigate, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./features/Login";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   const navigate = useNavigate();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 0,
+      },
+    },
+  });
   return (
-    <NextUIProvider navigate={navigate}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<p>Signup</p>} />{" "}
-        <Route path="reset" element={<p>Reset</p>} />
-        <Route path="app" element={<p>App</p>}>
-          <Route index element={<Navigate to={"travels"} replace />} />
-          <Route path="form" element={<p>Form</p>} />
-          <Route path="travels" element={<p>Travels</p>} />
-        </Route>
-        <Route path="*" element={<p>404, No page found</p>} />
-      </Routes>
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <NextUIProvider navigate={navigate}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<p>Signup</p>} />{" "}
+          <Route path="reset" element={<p>Reset</p>} />
+          <Route path="app" element={<p>App</p>}>
+            <Route index element={<Navigate to={"travels"} replace />} />
+            <Route path="form" element={<p>Form</p>} />
+            <Route path="travels" element={<p>Travels</p>} />
+          </Route>
+          <Route path="*" element={<p>404, No page found</p>} />
+        </Routes>
+      </NextUIProvider>
+    </QueryClientProvider>
   );
 }
 
