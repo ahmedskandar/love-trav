e5a660f9acddf993c50e2b1dd77fa121a315a47d
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-22 13:31:44 +0300

    feat: Add table column constant

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,1 +1,32 @@
 export const apiKey = import.meta.env.VITE_SUPABASE_API_KEY as string;
+
+export const COLUMNS = [
+  {
+    key: "id",
+    label: "ID",
+  },
+  {
+    key: "name",
+    label: "NAME",
+  },
+  {
+    key: "created_at",
+    label: "CREATED AT",
+  },
+  {
+    key: "password",
+    label: "PASSWORD",
+  },
+  {
+    key: "nationality",
+    label: "NATIONALITY",
+  },
+  {
+    key: "notes",
+    label: "NOTES",
+  },
+  {
+    key: "status",
+    label: "STATUS",
+  },
+];
