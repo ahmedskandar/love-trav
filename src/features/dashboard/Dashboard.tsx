666efc2f09ae6d1abe4a987acffb5a12bbabf36e
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-22 11:19:55 +0300

    feat: Implement Dashboard component

---
+++ src/features/dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import UserTable from "./UserTable";
+
+const Dashboard = () => {
+  return (
+    <>
+      <h2>Welcome Ahmed</h2>
+      <UserTable />
+    </>
+  );
+};
+
+export default Dashboard;
