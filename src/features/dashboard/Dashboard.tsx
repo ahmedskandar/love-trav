7acfa9ad9f08088ee64fddb4017f1aa03ab3e783
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-22 18:42:06 +0300

    style: Enhance responsiveness

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -4,7 +4,7 @@ const Dashboard = () => {
   return (
     <>
       <h2>Welcome Ahmed</h2>
-      <div className="mx-5">
+      <div className="px-5 sm:mx-auto sm:max-w-4xl xl:max-w-6xl">
         <UserTable />
       </div>
     </>
