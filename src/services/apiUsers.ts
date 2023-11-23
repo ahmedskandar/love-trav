165c9e30017ee09ce6de938e0b840971433aeef4
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-23 07:42:27 +0300

    feat: Implement delete user functionality

--- src/services/apiUsers.ts
+++ src/services/apiUsers.ts
@@ -2,13 +2,22 @@ import { User } from "../lib/types";
 import supabase from "./supabase";
 
 export const getUsers = async () => {
+  const { data: user, error } = await supabase.from("users").select("*");
 
-  const { data: user, error } = await supabase.from("users").select("*")
-
-  if(error) {
-    throw new Error("Users could not be loaded")
+  if (error) {
+    throw new Error("Users could not be loaded");
   }
   const data = user as User[];
-  
-  return data
+
+  return data;
+};
+
+export const deleteUser = async (id: number) => {
+  const { data, error } = await supabase.from("users").delete().eq("id", id);
+
+  if (error) {
+    throw new Error("Account could not be deleted, please try again");
+  }
+
+  return data;
 };
