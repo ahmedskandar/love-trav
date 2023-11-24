f8f75c2a4efa13c3eba6d6e645251b3822afa133
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-24 15:44:09 +0300

    feat: Implement onSubmit function

--- src/features/signup/SignupForm.tsx
+++ src/features/signup/SignupForm.tsx
@@ -1,26 +1,35 @@
 import { useState } from "react";
 
 import ImageUpload from "../../ui/ImageUpload";
-import NationalitySelect from "../../ui/NationalitySelect";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Input } from "@nextui-org/react";
 import { faEye } from "@fortawesome/free-solid-svg-icons/faEye";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons/faEnvelope";
 import { faEyeSlash } from "@fortawesome/free-solid-svg-icons/faEyeSlash";
-import { useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import FormPrompt from "../../ui/FormPrompt";
+
+import NationalitySelect from "../../ui/NationalitySelect";
 
 const SignupForm = () => {
   const [isEyeVisible, setIsEyeVisible] = useState(false);
   const toggleEyeVisibility = () => setIsEyeVisible(!isEyeVisible);
 
-  const { register } = useForm();
+  const { register, handleSubmit, control, reset } = useForm();
 
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    //eslint-disable-next-line
+    const nationality = Array.from(data.nationality)[0]
+    console.log(nationality)
+
+    reset()
+  };
   return (
-    <form className="mt-8">
+    //eslint-disable-next-line
+    <form className="mt-8" onSubmit={handleSubmit(onSubmit)}>
       <Input
-        {...register("name")}
+        {...register("username")}
         isRequired
         type="text"
         variant="bordered"
@@ -67,7 +76,7 @@ const SignupForm = () => {
         type={isEyeVisible ? "text" : "password"}
       />
 
-      <NationalitySelect />
+      <NationalitySelect control={control} />
 
       <ImageUpload />
 
