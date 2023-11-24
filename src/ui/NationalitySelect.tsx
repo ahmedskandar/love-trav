0d6f2f7a94cc2cb4899056f7de1217e83d1cd959
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-24 15:45:09 +0300

    feat: Transform compoennt to controlled component

--- src/ui/NationalitySelect.tsx
+++ src/ui/NationalitySelect.tsx
@@ -1,14 +1,23 @@
+import { Country, NationalitySelectProps } from "../lib/types";
+
 import { Select, SelectItem, Avatar } from "@nextui-org/react";
 import { useQuery } from "@tanstack/react-query";
-import { Country } from "../lib/types";
+import { Controller } from "react-hook-form";
 
-const NationalitySelect = () => {
+const NationalitySelect = ({ control }: NationalitySelectProps) => {
   const fetchCountries = async () => {
     const res = await fetch("https://flagcdn.com/en/codes.json");
-    if (!res.ok) throw new Error("Smething wrong happened");
+    if (!res.ok) throw new Error("Countries can't fetch, please try again");
     const data = (await res.json()) as Country;
-    if (!data) throw new Error("Country is empty");
-    return data;
+    if (Object.keys(data).length === 0) throw new Error("Countries can't be fetched, please try again");
+    
+    //Converts the data object into array  objects with key, value pairs
+    const dataArray = Object.entries(data).map(([key, value]) => ({
+      key,
+      value,
+    }));
+
+    return dataArray;
   };
 
   const { isLoading, data, error } = useQuery({
@@ -16,54 +25,62 @@ const NationalitySelect = () => {
     queryFn: fetchCountries,
   });
 
-  const countryArray = data ? Object.entries(data) : [];
-  const sortedCountryArray = countryArray.sort((a, b) =>
-    a[1].localeCompare(b[1]),
-  );
+
   return (
-    <Select
-      isRequired
-      isInvalid={!!error?.message}
-      isLoading={isLoading}
-      errorMessage={error?.message}
-      items={sortedCountryArray}
-      color="warning"
-      label="Nationality"
-      labelPlacement="outside"
-      className="mb-5"
-      classNames={{
-        trigger: "h-14",
-      }}
-      variant="bordered"
-      renderValue={(items) => {
-        return items.map((item) => (
-          <div key={item.key} className="flex items-center gap-2">
-            <Avatar
-              alt={item.textValue}
-              className="flex-shrink-0"
-              size="sm"
-              src={`https://flagcdn.com/${item.key}.svg`}
-            />
-            <span>{item.textValue}</span>
-          </div>
-        ));
-      }}
-    >
-      {(country) => (
-        <SelectItem
-          key={country[0]} // Assuming the country key is the first element in the tuple
-          startContent={
-            <Avatar
-              alt={country[1]} // Assuming the country value is the second element in the tuple
-              className="h-6 w-6"
-              src={`https://flagcdn.com/${country[0]}.svg`}
-            />
-          }
+    <Controller
+      name="nationality"
+      control={control}
+      defaultValue={[]}
+      rules={{ required: true }} // Add this line
+      render={({ field }) => (
+        <Select
+          onSelectionChange={field.onChange}
+          // eslint-disable-next-line
+          selectedKeys={field.value}
+          isRequired
+          isInvalid={!!error?.message}
+          isLoading={isLoading}
+          errorMessage={error?.message}
+          items={data ?? []}
+          color="warning"
+          label="Nationality"
+          labelPlacement="outside"
+          className="mb-5"
+          classNames={{
+            trigger: "h-14",
+          }}
+          variant="bordered"
+          renderValue={(items) => {
+            return items.map((item) => (
+              <div key={item.key} className="flex items-center gap-2">
+                <Avatar
+                  alt={item.textValue}
+                  className="flex-shrink-0"
+                  size="sm"
+                  src={`https://flagcdn.com/${item.key}.svg`}
+                />
+                <span>{item.textValue}</span>
+              </div>
+            ));
+          }}
         >
-          {country[1]}
-        </SelectItem>
+          {(country) => (
+            <SelectItem
+              key={country.key}
+              startContent={
+                <Avatar
+                  alt={country.value}
+                  className="h-6 w-6"
+                  src={`https://flagcdn.com/${country.key}.svg`}
+                />
+              }
+            >
+              {country.value}
+            </SelectItem>
+          )}
+        </Select>
       )}
-    </Select>
+    />
   );
 };
 
