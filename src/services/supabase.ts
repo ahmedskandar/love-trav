ed915cff72e349a2ae190b894d114248052ab4b1
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-24 20:14:41 +0300

    chore: Export supabase url

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { apiKey } from "../data/constants";
 
-const supabaseUrl = "https://dzypyxpbrupmlvqsxdmw.supabase.co";
+export const supabaseUrl = "https://dzypyxpbrupmlvqsxdmw.supabase.co";
 const supabaseKey = apiKey; //! will tell TypeScript that process.env.SUPABASE_KEY is not null or undefined
 const supabase = createClient(supabaseUrl, supabaseKey);
 
