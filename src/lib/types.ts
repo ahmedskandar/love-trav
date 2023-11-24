cbc38bdb4803551b4c1cc5e1158eac9291bfcae2
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-24 06:50:03 +0300

    feat: Implement Heading component type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -27,3 +27,13 @@ export type Country = Record<string, string>;
 export type FormPrompt = {
   to: "login" | "signup";
 };
+
+export type Children = {
+  children: React.ReactNode
+};
+
+export type ClassName = {
+  className?: string
+}
+
+export type Heading = Children & ClassName
