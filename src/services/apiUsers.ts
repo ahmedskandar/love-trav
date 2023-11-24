1bd57e28246a011542eacc0a7cc394ac8982dd78
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-24 20:14:57 +0300

    feat: Implement image storage

--- src/services/apiUsers.ts
+++ src/services/apiUsers.ts
@@ -1,5 +1,5 @@
-import { User } from "../lib/types";
-import supabase from "./supabase";
+import { FormData, User } from "../lib/types";
+import supabase, { supabaseUrl } from "./supabase";
 
 export const getUsers = async () => {
   const { data: user, error } = await supabase.from("users").select("*");
@@ -18,16 +18,29 @@ export const deleteUser = async (id: number) => {
   if (error) {
     throw new Error("Account could not be deleted, please try again");
   }
-
 };
 
-export const createUser = async (newUser: {username: string, email: string, password: string, nationality: string}) => {
-  const { error } = await supabase
+export const createUser = async (newUser: FormData) => {
+  const imageName = `${Math.random()}-${newUser.image.name}`.replace("/", "");
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/avatars/${imageName}`;
+  // 1. Create user
+  const { data, error } = await supabase
     .from("users")
-    .insert([newUser])
+    .insert([{ ...newUser, image: imagePath }]);
 
   if (error) {
-    throw new Error("User could not be deleted, please try again");
+    throw new Error("User could not be added, please try again");
   }
 
+  // 2. Upload image
+  // Upload file using standard upload
+  // eslint-disable-next-line
+  const { error: storageError } = await supabase.storage.from("avatars").upload(imageName, newUser.image);
+  if (storageError) {
+    // Delete cabin
+    await supabase.from("users").delete().eq("id", data.id);
+    throw new Error("Image could not be uploaded, please try again");
+  } else {
+    // Handle success
+  }
 };
