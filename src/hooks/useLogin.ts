7e97a217da8e618cc55f07e7e37ddd6bdcf15dd7
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-30 08:37:10 +0300

    feat: Add toaster

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,14 +1,18 @@
 import { useMutation } from "@tanstack/react-query";
 import { login as loginAPI } from "../services/apiAuth";
 import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 export const useLogin = () => {
   const navigate = useNavigate();
   const { mutate: login, isPending } = useMutation({
     mutationFn: ({ email, password }: { email: string; password: string }) =>
       loginAPI({ email, password }),
-    onSuccess: () => navigate("/app/travels", { replace: true }), //Show toast and navigate
-    onError: (err) => alert(err.message + "WRONG EMAIL/PASS COMBO"),
+    onSuccess: () => {
+      toast.success("Successfully logged in")
+      navigate("/app/travels");
+    }, //Show toast and navigate
+    onError: (error) => toast.error(error.message),
   });
 
   return { login, isPending };
--- src/hooks/useSignup.ts
+++ src/hooks/useSignup.ts
@@ -1,11 +1,18 @@
 import { useMutation } from "@tanstack/react-query";
 import { signup as signupAPI } from "../services/apiAuth";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
 export const useSignup = () => {
+  const navigate = useNavigate()
   const { mutate: signup, isPending } = useMutation({
     mutationFn: signupAPI,
-    onSuccess: () => alert("Account successfully created"),
-    onError: (err) => alert(err.message),
+    //fix onsuccess here and when its used: just use 1 which is here
+    onSuccess: () => {
+      toast.success("Account successfully created");
+      navigate('/login')
+    },
+    onError: (error) => toast.error(error.message),
   });
 
   return { signup, isPending };
