325f62d9c6d5491f13f7864a9890819583627ced
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-30 08:36:54 +0300

    feat: Implement useCountries hook

---
+++ src/hooks/useCountries.ts
@@ -0,0 +1,70 @@
+import React from "react";
+
+export const useCountries = ({ fetchDelay = 0 }) => {
+  const [items, setItems] = React.useState<{key: string, value: string}[]>([]);
+  const [hasMore, setHasMore] = React.useState(true);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [offset, setOffset] = React.useState(0);
+  const limit = 20; // Number of items per page, adjust as necessary
+
+  const loadCountries = React.useCallback(async () => {
+    try {
+      const controller = new AbortController();
+      const { signal } = controller;
+      setIsLoading(true);
+      await new Promise((resolve) => setTimeout(resolve, fetchDelay));
+
+      const res = await fetch("https://flagcdn.com/en/codes.json", {
+        signal,
+      });
+
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await res.json() as Record<string, string>;
+      const dataArray = Object.entries(data).map(([key, value]) => ({
+        key,
+        value,
+      }));
+
+      const paginatedItems = dataArray.slice(offset, offset + limit);
+
+      setHasMore(offset + limit < dataArray.length);
+      setItems((prevItems) => {
+        const combinedItems = [...prevItems, ...paginatedItems];
+        const uniqueItemsSet = new Set(combinedItems.map((item) => item.key));
+        const uniqueItemsArray = Array.from(uniqueItemsSet).map((key) =>
+          combinedItems.find((item) => item.key === key),
+        );
+
+        return uniqueItemsArray as { key: string; value: string }[];
+      });
+    } catch (error) {
+      if (error instanceof Error && error.name === "AbortError") {
+        console.log("Fetch aborted");
+      } else {
+        console.error("There was an error with the fetch operation:", error);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }, [offset, fetchDelay]);
+
+  React.useEffect(() => {
+    void loadCountries();
+  }, [loadCountries]);
+
+  const onLoadMore = () => {
+    const newOffset = offset + limit;
+
+    setOffset(newOffset);
+    void loadCountries();
+  };
+
+  return {
+    items,
+    hasMore,
+    isLoading,
+    onLoadMore,
+  };
+}
