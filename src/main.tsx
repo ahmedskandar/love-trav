1e3bad3155e86be0f1252066a0188f8527cd4a91
Ahmed Lukman <itsahmedlukman@gmail.com>
2023-11-30 08:35:26 +0300

    feat: Implement toaster

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster position="top-center" richColors/>
     </BrowserRouter>
   </React.StrictMode>,
 );
